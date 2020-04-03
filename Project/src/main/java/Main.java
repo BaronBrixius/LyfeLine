158098a8b87738c005c81cc469c7a60ec95b604a
Haraldur Blöndal Kristjánsson <hk222su@student.lnu.se>
2020-04-03 11:04:27 +0000

    Revert "Merge branch 'Haraldur' into 'master'"
    This reverts merge request !11

--- Project/src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-package main.java;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.print("Hello Purple!");
-        System.out.print("My name is Haraldur Blöndal Kristjánsson");
-    }
-}
