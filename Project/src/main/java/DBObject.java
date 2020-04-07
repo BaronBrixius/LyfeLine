6b5be80ae2513cc367b96b374d8cc64e6bb28166
Max Anderson <ma225bj@student.lnu.se>
2020-04-07 11:25:42 +0000

    Revert "Basic read/write interface for java/sql"
    This reverts commit a19f34fa3979155893a2be3c3b4841684465120d

--- Project/src/main/java/DBObject.java
+++
@@ -1,7 +0,0 @@
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-public interface DBObject<T> {
-    T createFromDB(ResultSet rs) throws SQLException;
-    String getInsertQuery() throws SQLException;
-}
--- Project/src/main/java/Main.java
+++ Project/src/main/java/Main.java
@@ -1,17 +1,5 @@
-import java.sql.SQLException;
-
 public class Main {
-    public static void main(String[] args) throws SQLException {
-        DBManager db = null;
-        try {
-            db = new DBManager();
-
-            Event now = new Event();
-            System.out.print(now.getInsertQuery());
-            db.insertIntoDB(now);
-        } finally {
-            if (db != null)
-                db.close();
-        }
+    public static void main(String[] args) {
+        System.out.println("Hello Purple!");
     }
 }
