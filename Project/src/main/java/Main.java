bac6f95c6ee9d6ed7a787dec7340bfd0dc70eb4d
Max Anderson <ma225bj@student.lnu.se>
2020-04-07 13:49:14 +0200

    maybe fixed gitignore now

--- Project/src/main/java/Main.java
+++ Project/src/main/java/Main.java
@@ -1,5 +1,17 @@
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello Purple!");
+    public static void main(String[] args) throws SQLException {
+        DBManager db = null;
+        try {
+            db = new DBManager();
+
+            Event now = new Event();
+            System.out.print(now.getInsertQuery());
+            db.insertIntoDB(now);
+        } finally {
+            if (db != null)
+                db.close();
+        }
     }
 }
