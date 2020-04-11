b88bbc2c84258ee5fca0e0d089ff06ff4a9292f4
Lorenz Steiner <ls223tx@student.lnu.se>
2020-04-11 13:43:59 +0200

    initial commit test

---
+++ Project/src/main/java/TimelineList_GUI.java
@@ -0,0 +1,17 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TimelineList_GUI extends Application{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
