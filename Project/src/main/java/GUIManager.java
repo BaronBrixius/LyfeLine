5506ac2bbcdeec0d4361637a44a0e08ffbc58321
Ben Anderson <ba222kc@student.lnu.se>
2020-04-15 18:46:08 +0200

    Added support for CSS Style Sheets and improved visual quality of GUIs

--- Project/src/main/java/GUIManager.java
+++ Project/src/main/java/GUIManager.java
@@ -2,6 +2,8 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.sql.SQLException;
+
 public class GUIManager extends Application{
 
 	public static void main(String[] args) {
@@ -9,18 +11,36 @@ public class GUIManager extends Application{
 	}
 	
 	public static Stage mainStage;
+	public static String mainStyle;
 	
 	//default window set up
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+
+		// Used to establish connection to the DB.
+		try {
+			new DBM();
+			DBM.setupSchema();
+		}
+		catch (SQLException e) {
+			System.out.println("Could not establish the connection to the SQL server");
+		}
+
 		mainStage = primaryStage;
 		mainStage.setScene(LoginAndRegistration_GUI.welcomeScreen()); //default scene
 		mainStage.setResizable(false);
+		changeStyle("DefaultStyle");
 		mainStage.show();
 	}
 	
 	//is used when swapping scenes inside classes. use the static classes that return scenes
 	public static void swapScene(Scene scene) {
 		mainStage.setScene(scene);
+		changeStyle(mainStyle);
+	}
+
+	public static void changeStyle(String styleName) {
+		mainStyle = styleName;
+		mainStage.getScene().getStylesheets().add("File:src/main/resources/"+ mainStyle +".css");
 	}
 }
