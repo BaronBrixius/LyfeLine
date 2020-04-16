e29005bc7307f2c669e9eb07acdab66f81deb33e
Ben Anderson <ba222kc@student.lnu.se>
2020-04-16 11:52:37 +0200

    Cleaned up the ToggleSwitch code

--- Project/src/main/java/ToggleSwitch.java
+++ Project/src/main/java/ToggleSwitch.java
@@ -1,5 +1,6 @@
 import javafx.animation.FillTransition;
 import javafx.animation.ParallelTransition;
+import javafx.animation.Timeline;
 import javafx.animation.TranslateTransition;
 import javafx.application.Application;
 import javafx.beans.binding.Bindings;
@@ -7,6 +8,7 @@ import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
@@ -17,61 +19,51 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
-	/* Credit & source: https://github.com/AlmasB/FXTutorials/blob/master/src/com/almasb/ios/IOSApp.java
-	 * This is a custom made toggle switch that has been modified to our need in the purple group.
-	 */
-    public class ToggleSwitch extends Parent {
+/* Credit & source: https://github.com/AlmasB/FXTutorials/blob/master/src/com/almasb/ios/IOSApp.java
+ * This is a custom made toggle switch that has been modified to our need in the purple group.
+ */
+public class ToggleSwitch extends Parent {
 
-        private BooleanProperty switchedOn = new SimpleBooleanProperty(false);
+    public BooleanProperty switchedOn = new SimpleBooleanProperty(false);
 
-        private TranslateTransition translateAnimation = new TranslateTransition(Duration.seconds(0.25));
-        private FillTransition fillAnimation = new FillTransition(Duration.seconds(0.25));
+    public ToggleSwitch() {
+        Rectangle background = new Rectangle(100, 50);
+        background.setArcWidth(50);
+        background.setArcHeight(50);
+        background.setFill(Color.WHITE);
+        background.setStroke(Color.LIGHTGRAY);
 
-        private ParallelTransition animation = new ParallelTransition(translateAnimation, fillAnimation);
+        Circle trigger = new Circle(25, 25, 25);
+        trigger.setFill(Color.WHITE);
+        trigger.setStroke(Color.LIGHTGRAY);
+        trigger.setEffect(new DropShadow(2, Color.valueOf("0x000000ff")));
 
-        public BooleanProperty switchedOnProperty() {
-            return switchedOn;
-        }
+        TranslateTransition translateAnimation = new TranslateTransition(Duration.seconds(0.25));
+        translateAnimation.setNode(trigger);
 
-        public ToggleSwitch() {
-            Rectangle background = new Rectangle(100, 50);
-            background.setArcWidth(50);
-            background.setArcHeight(50);
-            background.setFill(Color.WHITE);
-            background.setStroke(Color.LIGHTGRAY);
+        FillTransition fillAnimation = new FillTransition(Duration.seconds(0.25));
+        fillAnimation.setShape(background);
 
-            Circle trigger = new Circle(25);
-            trigger.setCenterX(25);
-            trigger.setCenterY(25);
-            trigger.setFill(Color.WHITE);
-            trigger.setStroke(Color.LIGHTGRAY);
+        ParallelTransition animation = new ParallelTransition(translateAnimation, fillAnimation);
 
-            DropShadow shadow = new DropShadow();
-            shadow.setRadius(2);
-            trigger.setEffect(shadow);
+        getChildren().addAll(background, trigger);
 
-            translateAnimation.setNode(trigger);
-            fillAnimation.setShape(background);
+        switchedOn.addListener((obs, oldState, newState) -> {
+            setDisable(true);
 
-            getChildren().addAll(background, trigger);
+            translateAnimation.setToX(newState ? 100 - 50 : 0);
+            fillAnimation.setFromValue(newState ? Color.WHITE : Color.LIGHTGREEN);
+            fillAnimation.setToValue(newState ? Color.LIGHTGREEN : Color.WHITE);
+            trigger.setFill(newState ? Color.DARKRED : Color.WHITE);
 
-            switchedOn.addListener((obs, oldState, newState) -> {
-                boolean isOn = newState;
-                
-                translateAnimation.setToX(isOn ? 100 - 50 : 0);
-                fillAnimation.setFromValue(isOn ? Color.WHITE : Color.LIGHTGREEN);
-                fillAnimation.setToValue(isOn ? Color.LIGHTGREEN : Color.WHITE);
-               
-                trigger.setFill(isOn ? Color.WHITE : Color.DARKRED);
+            animation.play();
+            animation.setOnFinished(e -> setDisable(false));
+        });
 
-                animation.play();
-            });
+        setOnMouseClicked(event -> { // add functionality here
+            switchedOn.setValue(!switchedOn.get());
+        });
 
-            setOnMouseClicked(event -> { // add functionality here
-                switchedOn.set(!switchedOn.get());
-            });
-        
-        }
-        
-        
     }
+
+}
