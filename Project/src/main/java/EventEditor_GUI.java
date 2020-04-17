83dc88079e597a6e1ed4e56031c90e4ff8696e85
Max Anderson <ma225bj@student.lnu.se>
2020-04-17 15:26:43 +0200

    rough draft event editor gui

--- Project/src/main/java/EventEditor_GUI.java
+++ Project/src/main/java/EventEditor_GUI.java
@@ -1,14 +1,13 @@
-import javafx.geometry.HPos;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.*;
-import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class EventEditor_GUI extends VBox {
-    Label header = new Label("Event Editor");
+    Label nameLabel = new Label("New Event");
     Label titleLabel = new Label("Title");
     Label descriptionLabel = new Label("Description");
     Label startLabel = new Label("Start");
@@ -31,55 +30,76 @@ public class EventEditor_GUI extends VBox {
     }
 
     public EventEditor_GUI(Event event) {
-        Label[] labels = new Label[]{titleLabel, descriptionLabel, startLabel, endLabel, imageLabel};
-
-        GridPane buttons = new GridPane();
-        buttons.addColumn(0, uploadButton, saveButton, deleteButton);
-        buttons.add(closeButton, 1, 2);
-
-        for (Node b: buttons.getChildren())
-            b.getStyleClass().add("smallButton");
-        saveButton.setPadding(new Insets(50,50,50,50));
-
         this.event = event;
-        setScaleX(0.9);
-        setScaleY(0.9);
 
+        setScaleX(0.9); setScaleY(0.9);
+        setPrefWidth(350);
 
-        setSpacing(5);
+        //nameLabel.setText(event.getEventName);    event name not implemented yet
+        nameLabel.setAlignment(Pos.CENTER);
+        nameLabel.setStyle("-fx-font-size: 2em");
 
+        //End picker group
+        HBox endHeader = new HBox(5);
+        hasDuration.setPadding(new Insets(5, 0, 0, 0));
+        endHeader.getChildren().addAll(hasDuration, endLabel);
 
+        //Image selection group
+        HBox imageChooser = new HBox(10);
+        imageChooser.getChildren().addAll(imageInput, uploadButton);
+        imageInput.setPrefWidth(200);
 
-        hasDuration.setPadding(new Insets(5,0,0,0));
+        //Buttons at bottom group
+        GridPane buttons = new GridPane();
+        buttons.addColumn(0, saveButton, deleteButton);
+        buttons.add(closeButton, 1, 1);
+        buttons.setPadding(new Insets(10,0,0,0));
 
-        HBox endHeader = new HBox(50);
-        endHeader.getChildren().addAll(endLabel, hasDuration);
+        for (Node b : buttons.getChildren())
+            b.getStyleClass().add("smallButton");
 
-        //HBox bottom
+        uploadButton.setStyle("-fx-pref-width: 100px;" +
+                "    -fx-pref-height: 30px;" +
+                "    -fx-font-size: 1em;");
 
-        getChildren().addAll(header,
+        //Add to window
+        getChildren().addAll(nameLabel,
                 titleLabel, titleInput,
                 descriptionLabel, descriptionInput,
                 startLabel, startInput, endHeader, endInput,
-                imageLabel, imageInput, buttons);
-        //add(closeButton, 1, 13);
+                imageLabel, imageChooser, buttons);
 
-
-        setHasDuration(false);
-
+        //Define actions
+        endInput.setDisable(true);
         hasDuration.setOnAction(e ->
-                setHasDuration(hasDuration.isSelected()));
+                endInput.setDisable(!hasDuration.isSelected())
+        );
 
+        uploadButton.setOnAction(e ->
+                uploadImage()
+        );
 
+        saveButton.setOnAction(e ->
+                saveEvent()
+        );
 
-        updateDisplay();
+        deleteButton.setOnAction(e ->
+                deleteEvent()
+        );
+
+        closeButton.setOnAction(e ->
+                close()
+        );
+
+        populateDisplay();
     }
 
-    private void setHasDuration(boolean set) {
-        endInput.setDisable(!set);
+    private void uploadImage() {
+        //don't implement, not part of current sprint
+        System.out.println("Button pressed.");
     }
 
-    public boolean changeEvent(int eventID) {
+    public boolean changeEvent(int eventID) {       //is this even needed? don't implement yet
         /*Event newEvent = logic to find Event in database and get its info
         if (newEvent != null)
             return changeEvent(newEvent);*/
@@ -87,18 +107,17 @@ public class EventEditor_GUI extends VBox {
         return false;
     }
 
-    public boolean changeEvent(Event event) {
+    public boolean changeEvent(Event event) {       //is this even needed? don't implement yet
         this.event = event;
-        return updateDisplay();
+        return populateDisplay();
     }
 
-    private boolean updateDisplay() {
+    private boolean populateDisplay() {
         //populate fields with info from event, or leave blank if new event and such
-
         return false;
     }
 
-    public boolean saveToDB() {
+    private boolean saveEvent() {
         //setters to update each field of this.event, based on the current info in the text fields
 
         /*try {
@@ -110,7 +129,20 @@ public class EventEditor_GUI extends VBox {
         } catch (SQLException e){
             return false;
         }*/
-
+        System.out.println("Button pressed.");
         return false;
+    }
+
+    private boolean deleteEvent() {
+        // prompt if user wants to continue
+        // delete event from DB, on this and all other timelines
+        System.out.println("Button pressed.");
+        return false;
+    }
+
+
+    private void close() {
+        //close editor, return to previous screen
+        System.out.println("Button pressed.");
     }
 }
