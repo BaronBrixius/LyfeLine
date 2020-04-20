f60085bc68c93797cff67a6fc6c38e7475b79e33
Matas Pipiras <mp223gr@student.lnu.se>
2020-04-20 16:33:00 +0200

    FXML functionality added

--- Project/src/main/java/GUIManager.java
+++ Project/src/main/java/GUIManager.java
@@ -1,9 +1,10 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -49,7 +50,7 @@ public class GUIManager extends Application {
         mainStage = primaryStage;
         mainStage.setScene(new Scene(main));
 
-        swapScene("EventEditor");
+        swapScene("Dashboard");
         applyStyle("DefaultStyle");
 
         mainStage.show();
