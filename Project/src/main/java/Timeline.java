90aaa31c4b758627038c360802727065756c8335
Max Anderson <ma225bj@student.lnu.se>
2020-04-21 13:51:51 +0200

    cleaning for push... again

--- Project/src/main/java/Timeline.java
+++ Project/src/main/java/Timeline.java
@@ -20,7 +20,7 @@ public class Timeline implements DBObject<Timeline>{
 
 	}
 
-	public Timeline(int TimeLineID, String TimelineName, String TimelineDescription, String Scale, String Theme, Date StartDate, Date Enddate, Date DateCreated, int TimelineOwner, boolean Private) throws SQLException {
+	private Timeline(int TimeLineID, String TimelineName, String TimelineDescription, String Scale, String Theme, Date StartDate, Date Enddate, Date DateCreated, int TimelineOwner, boolean Private) throws SQLException {
 
 		this.timelineID=TimeLineID;
 		this.timelineName = TimelineName;
