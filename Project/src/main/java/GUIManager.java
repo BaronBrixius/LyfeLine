a7213c4d4ace087e557553fc3b691c35f28f9dda
Max Anderson <ma225bj@student.lnu.se>
2020-04-22 08:22:06 +0000

    Update GUIManager.java

--- Project/src/main/java/GUIManager.java
+++ Project/src/main/java/GUIManager.java
@@ -39,8 +39,8 @@ public class GUIManager extends Application {
 
         // Used to establish connection to the DB.
         try {
-            new DBM("jdbc:mysql://localhost?useTimezone=true&serverTimezone=UTC", "root", "AJnuHA^8VKHht=uB", "project");
-            //DBM.setupSchema(); //comment out for testing of log in
+            new DBM();
+            DBM.setupSchema(); //comment out for testing of log in
         } catch (SQLException e) {
             e.printStackTrace();
         }
