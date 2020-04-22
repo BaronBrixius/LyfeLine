4a05fb17a6363c1e121cc7c3cae1dd2fb0215af3
Ben Anderson <ba222kc@student.lnu.se>
2020-04-22 13:47:30 +0200

    Worked on save button

--- Project/src/main/java/EventEditor_GUI.java
+++ Project/src/main/java/EventEditor_GUI.java
@@ -107,6 +107,8 @@ public class EventEditor_GUI {
 
     private boolean populateDisplay() {
 
+        event.setEndDate(new Date(4, 3, 4, 5, 3,4, 3));
+
         titleInput.setText(event.getEventName());
         descriptionInput.setText(event.getEventDescrition());
 
@@ -114,6 +116,13 @@ public class EventEditor_GUI {
             startDate.setValue(LocalDate.of(0, 1, 1));
         else
             startDate.setValue(LocalDate.of(event.getStartDate().getYear(), event.getStartDate().getMonth(), event.getStartDate().getDay()));
+
+        if (event.getStartDate() != event.getEndDate())
+        {
+            hasDuration.setSelected(true);
+            toggleHasDuration();
+            endDate.setValue(LocalDate.of(event.getEndDate().getYear(), event.getEndDate().getMonth(), event.getEndDate().getDay()));
+        }
 
         startTime1.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 59, event.getStartDate().getSeconds()));
         startTime2.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 59, event.getStartDate().getMinutes()));
@@ -158,8 +167,9 @@ public class EventEditor_GUI {
                 DBM.updateInDB(event);
             return true;
         } catch (SQLException e) {
-            return false;
+            e.printStackTrace();
         }
+        return true;
     }
 
     @FXML
