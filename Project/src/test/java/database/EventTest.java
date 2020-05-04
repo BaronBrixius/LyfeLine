56b56374352f61cc4d2f2ae65ea91cacd418485a
Firas Al Khateeb <fa222ts@student.lnu.se>
2020-05-04 15:09:45 +0200

    event is done

---
+++ Project/src/test/java/database/EventTest.java
@@ -0,0 +1,161 @@
+package database;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.*;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventTest {
+    static final private String SCHEMA = "test";
+    static private int testCount = 0;
+    Event test=new Event();
+    static Event[] events = new Event[5];
+
+    @BeforeAll
+    static void init() throws SQLException, IOException, ClassNotFoundException {
+        new DBM(SCHEMA);
+        DBM.setupSchema();
+    }
+
+    @AfterAll
+    static void finish() throws SQLException {
+        DBM.conn.createStatement().execute("DROP DATABASE IF EXISTS test");
+        DBM.conn.close();
+    }
+
+    @BeforeEach
+    void setUp() {
+        testCount++;
+        System.out.println("Test " + testCount);
+
+        try {
+            DBM.setupSchema();
+        } catch (SQLException | FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void getInsertQuery() throws SQLException {
+        Event test=new Event();
+        test.setID(1);
+        test.setImage("test");
+        test.setTitle("test");
+        test.setDescription("Test");
+        String sql = "INSERT INTO `events` (`EventID`, `ImagePath`, `EventName`, `EventDescription`) VALUES ('" + test.getEventID() + "','" + test.getImagePath() + "','" + test.getEventName() +"','" + test.getEventDescrition() + "',?)";
+        PreparedStatement out = DBM.conn.prepareStatement(sql   , Statement.RETURN_GENERATED_KEYS);
+        out.setInt(1, test.getUserID());
+        Exception exception = assertThrows(SQLIntegrityConstraintViolationException.class, () -> {
+            test.getInsertQuery();
+        });
+        assertNotNull(test);
+
+    }
+
+    @Test
+    void addToTimeline() throws SQLException {
+        PreparedStatement out = DBM.conn.prepareStatement("INSERT IGNORE INTO `timelineevents` (`TimelineID`, `EventID`) VALUES (?, ?);");
+        out.setInt(1, 0);
+        out.setInt(2, 0);
+        assertNotNull(out);
+    }
+
+
+    @Test
+    void createFromDB() throws SQLException {
+        Event test=new Event();
+        events[0]=test;
+        DBM.insertIntoDB(test);
+        Event test1=new Event();
+        events[1]=test1;
+        DBM.insertIntoDB(test1);
+        Event test2=new Event();
+        events[2]=test2;
+        DBM.insertIntoDB(test2);
+        assertEquals(test,events[0]);
+        assertEquals(test1,events[1]);
+        assertEquals(test2,events[2]);
+        /*
+        assertNotNull(1);
+        assertEquals("test.test",test.getImagePath());
+        assertEquals("test",test.getEventName());
+        assertEquals("Test",test.getEventDescrition());*/
+
+
+    }
+
+    @Test
+    void setID() {
+        test.setID(1);
+        assertEquals(1,1);
+    }
+
+    @Test
+    void setTitle() {
+        test.setTitle("Test");
+        assertEquals("Test","Test");
+    }
+
+    @Test
+    void setDescription() {
+        test.setDescription("Test");
+        assertEquals("Test","Test");
+    }
+
+    @Test
+    void setImage() {
+        test.setImage("Test");
+        assertEquals("Test","Test");
+    }
+
+    @Test
+    void getEventDescrition() {
+        test.getEventDescrition();
+        assertEquals("Test","Test");
+    }
+
+    @Test
+    void getUpdateQuery() throws SQLException {
+
+        Event test=new Event();
+        events[0]=test;
+        DBM.insertIntoDB(test);
+
+        DBM.updateInDB(test);
+
+    }
+
+    @Test
+    void getDeleteQuery() throws SQLException {
+        Event test=new Event();
+        events[0]=test;
+        DBM.insertIntoDB(test);
+        DBM.deleteFromDB(test);
+
+    }
+
+    @Test
+    void setUserID() {
+        test.setUserID(1);
+        assertEquals(1,1);
+    }
+    @Test
+    void close() throws SQLException, ClassNotFoundException {
+        DBM.conn.close();
+
+        assertThrows(SQLException.class, () -> DBM.conn.createStatement());
+        new DBM(SCHEMA);
+    }
+
+}
