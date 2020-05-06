9b2e37d356b3d4fd7b8fb6db5421e1ec923486d6
Max Anderson <ma225bj@student.lnu.se>
2020-05-06 22:32:41 +0200

    got Gson working as a test p quickly

--- Project/src/main/java/Main.java
+++ Project/src/main/java/Main.java
@@ -1,3 +1,4 @@
+import com.google.gson.Gson;
 import database.DBM;
 import database.Timeline;
 import database.User;
@@ -5,31 +6,47 @@ import utils.Date;
 import database.Event;
 
 import javax.swing.*;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.sql.Array;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
+import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
         PreparedStatement stmt;
         PreparedStatement stmt2;
         try {
-            new DBM(   );
+            new DBM();
             DBM.setupSchema();       //destroys + remakes DB with default settings, can comment this out after first run if desired
+
+
 
             //Makes a list of events from the DB and prints it
             stmt = DBM.conn.prepareStatement("SELECT * FROM events");
             List<Event> eventList = DBM.getFromDB(stmt, new Event());           //blank object so functional interface method can be accessed
-            System.out.println("\nEvent List:");
-            for (Event e : eventList)
-                System.out.println(e);
 
 
 
+            Gson json = new Gson();
+            String out = json.toJson(eventList.get(0));
+            System.out.println(out + "\n");
+            File file = new File("jsonTest.json");
+            PrintWriter outFile = new PrintWriter(file);
+            outFile.println(out);
+            outFile.close();
 
+            Scanner inFile = new Scanner(file);
+            Event readJson = json.fromJson(inFile.nextLine(), Event.class);
+            System.out.println(readJson + "\n");
+            System.out.println("Year: " + readJson.getStartDate().getYear());
+            inFile.close();
+/*
             //Makes a list of event years from the DB and prints it
             //note: you can just prepare a statement right in the method parameters if there aren't any field values that need to be set
             System.out.println("\nYear List:");
