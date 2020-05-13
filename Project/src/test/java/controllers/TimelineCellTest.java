ef493185a6bb15b2bdec28920b1dc20d79f36fac
Dillon Jonsson <dj222jm@student.lnu.se>
2020-05-13 11:33:47 +0200

    Limited testing to ratings for now

--- Project/src/test/java/controllers/TimelineCellTest.java
+++ Project/src/test/java/controllers/TimelineCellTest.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Semaphore;
 
 import org.junit.jupiter.api.AfterAll;
@@ -86,11 +87,14 @@ class TimelineCellTest {
 	}
 
 	@Test
-	void opacityTest() {
-
-		double actualOpacity = tc.ratingBox.getOpacity(); // checking opacity is 0 fora low rating
-		double expectedOpacity = 0;
-		assertEquals(actualOpacity, expectedOpacity);
+	void opacityTest() throws SQLException {
+		/*
+		 * TimelineCell testingCell = new TimelineCell();
+		 * //testingCell.setTimeline(timeline, width); double actualOpacity =
+		 * tc.ratingBox.getOpacity(); // checking opacity is 0 fora low rating double
+		 * expectedOpacity = 0; assertEquals(actualOpacity, expectedOpacity);
+		 */
+		// can not find a way to access the timelineCell for a timeline
 
 	}
 
@@ -109,24 +113,31 @@ class TimelineCellTest {
 	}
 
 	@Test
-	void LowRatingInDBTest() throws SQLException {
-		// Rating of 1 in db
+	void RatingTest() throws SQLException {
+		// Rating 1-5 for each timeline
 		GUIManager.main = new BorderPane(); // Avoids a null pointer?
 		ArrayList<Timeline> timelinesList = new ArrayList<>(dash.list.getItems());
 		int listSize = timelinesList.size();
 		for (int i = 0; i < listSize - 1; i++) {
-			dash.list.getSelectionModel().clearAndSelect(i);
-			Timeline timelineSelected = dash.list.getSelectionModel().getSelectedItem();
-			timelineSelected.addRating(1, 1);
-			double actual = timelineSelected.getRating();
-			double expected = 0;
-			assertEquals(actual, expected);
+			for (int j = 1; j < 5; j++) {
+				dash.list.getSelectionModel().clearAndSelect(i);
+				Timeline timelineSelected = dash.list.getSelectionModel().getSelectedItem();
+				timelineSelected.addRating(j, 1);
+				double actual = timelineSelected.getRating();
+				double expected = 0;
+				assertEquals(actual, expected);
+			}
 		}
 	}
 
 	@Test
-	void colorStarsByRatingTest() {
-
+	void ratingInDB() throws SQLException {
+		dash.list.getSelectionModel().clearAndSelect(3);
+		Timeline timelineSelected = dash.list.getSelectionModel().getSelectedItem();
+		timelineSelected.addRating(3, 1);
+		int expectedDB = DBM.getFromDB(DBM.conn.prepareStatement("SELECT COUNT(*) FROM rating "), rs -> rs.getInt(1)).get(0);
+		System.out.println(expectedDB);
+		assertEquals(expectedDB,1);
 	}
 
 	@Test
@@ -135,26 +146,21 @@ class TimelineCellTest {
 	}
 
 	@Test
-	void getTimelineTest() throws InterruptedException {
+	void getTimelineTest() throws InterruptedException, SQLException {
 		// need to find a way to iterate over all timelinecells
-		ArrayList<Timeline> timelinesList = new ArrayList<>(dash.list.getItems());
-		// ArrayList<TimelineCell> timelineCellList = new ArrayList<>(dash.time);
-		int listSize = timelinesList.size();
+		// ArrayList<Timeline> timelinesList = new ArrayList<>(dash.list.getItems());
+		int listSize = dash.list.getItems().size();
 		for (int i = 0; i < listSize - 1; i++) {
 			dash.list.getSelectionModel().select(i);
 			Timeline timelineSelected = dash.list.getSelectionModel().getSelectedItem();
 			String actual = timelineSelected.getName();
-			String expected = timelineSelected.getName();
+			String expected = dash.list.getSelectionModel().getSelectedItem().getName();
+			assertTrue(expected.contains(actual));
 		}
-		Timeline timelineSelected = dash.list.getSelectionModel().getSelectedItem();
-		tc.setTimeline(timelineSelected, 25); // Random Width for now
-		String actual = tc.timeline.getName();
-		String expected = tc.getTimeline().getName();
-		assertEquals(actual, expected);
 	}
 
 	@Test
-	void setTimelineTest() {
+	void setTimelineTest() throws SQLException {
 
 	}
 
@@ -165,6 +171,13 @@ class TimelineCellTest {
 
 	/* ********** Helper Methods ****************/
 	// Helper methods to make changing GUI elements possible
+	void testingCellsSetup() throws SQLException {
+		TimelineCell tc1 = new TimelineCell();
+		List<Timeline> timelineList = DBM.getFromDB(
+				DBM.conn.prepareStatement("SELECT * FROM timelines WHERE TimelineID = " + 1), new Timeline());
+		tc1.setTimeline(timelineList.get(0), 50);
+	}
+
 	void changeSortBy(int selection) throws InterruptedException {
 		Platform.runLater(() -> dash.sortBy.getSelectionModel().clearAndSelect(selection));
 		waitForRunLater();
