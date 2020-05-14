76d027d39c79300cdb63126654280853e0acdeb9
Max Anderson <ma225bj@student.lnu.se>
2020-05-14 12:11:52 +0200

    removed line

--- Project/src/main/java/controllers/Editor.java
+++ Project/src/main/java/controllers/Editor.java
@@ -258,7 +258,6 @@ public abstract class Editor {
 
         itemInEditor.setName(titleInput.getText());
         itemInEditor.setDescription(descriptionInput.getText().replaceAll("([^\r])\n", "$1\r\n"));
-        itemInEditor.setImage(imageFilePath);
 
         itemInEditor.setStartDate(new Date(startInputs.get(0).getValue(), startInputs.get(1).getValue(), startInputs.get(2).getValue(),
                 startInputs.get(3).getValue(), startInputs.get(4).getValue(), startInputs.get(5).getValue(), startInputs.get(6).getValue()));
