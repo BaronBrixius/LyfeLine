9aee09539ef3991014755f75c24ee17088a4014f
Max Anderson <ma225bj@student.lnu.se>
2020-05-26 00:14:45 +0200

    changing program startup back to normal page, my own personal dragon

--- Project/src/main/java/controllers/GUIManager.java
+++ Project/src/main/java/controllers/GUIManager.java
@@ -42,7 +42,7 @@ public class GUIManager {
     }
 
     public static void start(Stage stage) throws Exception {
-        loggedInUser = DBM.getFromDB(DBM.conn.prepareStatement("SELECT * FROM users"), new User()).get(0);  //TODO delete for final
+        //loggedInUser = DBM.getFromDB(DBM.conn.prepareStatement("SELECT * FROM users"), new User()).get(0);  //TODO delete for final
         main = new BorderPane();
         loader = new FXMLLoader(GUIManager.class.getResource("../FXML/TopMenu.fxml"));
         main.setTop(loader.load());
@@ -50,9 +50,9 @@ public class GUIManager {
 
         mainStage = stage;
         mainStage.setScene(new Scene(main));
-        //swapScene("LoginAndRegistration");
-        TimelineView systemUnderDevelopment = swapScene("TimelineView");        //TODO delete for final
-        systemUnderDevelopment.setActiveTimeline(DBM.getFromDB(DBM.conn.prepareStatement("SELECT * FROM timelines"), new Timeline()).get(0));
+        swapScene("LoginAndRegistration");
+        //TimelineView systemUnderDevelopment = swapScene("TimelineView");        //TODO delete for final
+        //systemUnderDevelopment.setActiveTimeline(DBM.getFromDB(DBM.conn.prepareStatement("SELECT * FROM timelines"), new Timeline()).get(0));
         mainStage.getScene().getStylesheets().add("File:src/main/resources/styles/Base.css");
         mainStage.getScene().getStylesheets().add("File:src/main/resources/styles/Default.css");
         FileInputStream icon = new FileInputStream("src/main/resources/LogoIcon.png");
